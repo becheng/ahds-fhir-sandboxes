2ff71e4ff35b20347db099b834d8215bf9951b18
becheng <ben.cheng@gmail.com>
2024-02-05 11:41:58 -0500

    ingest mvp

--- ingest-with-fhir-loader/scripts/seedWithSynthea.bash
+++ ingest-with-fhir-loader/scripts/seedWithSynthea.bash
@@ -3,7 +3,6 @@
 # Script has the following dependencies:
 # 1. zip
 # 2. azcopy
-# 3. synthea
 # ###############################################
 
 echo ""
@@ -13,7 +12,7 @@ echo ""
 while IFS='=' read -r key value; do
     value=$(echo "$value" | sed 's/^"//' | sed 's/"$//')
     export "$key=$value"
-    echo "$key=$value"
+    #echo "$key=$value"
 done <<EOF
 $(azd env get-values)
 EOF
@@ -44,8 +43,8 @@ echo "Preparing Synthea Patient Data...."
 echo ""
 
 # prompt patient number
-echo -e "\033[34mEnter the number of syntheic patients to generate (#) <press Enter to accept default> ["$defPatientNum"]:\033[0m"
-read patientNum 
+read -ep $'\033[34mEnter the number of syntheic patients to generate (#) <press Enter to accept default> ['$defPatientNum$']:\033[0m' patientNum
+#read patientNum 
 if [ -z "$patientNum" ] ; then
     patientNum=$defPatientNum
 fi
@@ -53,28 +52,29 @@ fi
 echo -e "\033[32mYou entered '$patientNum' patient(s).\033[0m"
 # validate the input is a number
 if ! [[ $patientNum =~ ^[0-9]+$ ]] ; then
-    echo -e "\033[31mInvalid entry. Enter a number.\033[0m"
+    echo -e "\033[31mInvalid entry. Enter a valid number.\033[0m"
     exit 0
 fi
 echo ""
 
 # other synthea options
 echo -e "\033[34mEnter other Synthea options <press Enter to skip>, e.g.
-         [-s seed]
-         [-cs clinicianSeed]
-         [-p populationSize]
-         [-r referenceDate as YYYYMMDD]
-         [-g gender]
-         [-a minAge-maxAge]
-         [-o overflowPopulation]
-         [-c localConfigFilePath]
-         [-d localModulesDirPath]
-         [-i initialPopulationSnapshotPath]
-         [-u updatedPopulationSnapshotPath]
-         [-t updateTimePeriodInDays]
-         [-f fixedRecordPath]
-         [-k keepMatchingPatientsPath]
-         [state [city]]:\033[0m"
+    [-s seed]
+    [-cs clinicianSeed]
+    [-p populationSize]
+    [-m syntheaModule]
+    [-r referenceDate as YYYYMMDD]
+    [-g gender]
+    [-a minAge-maxAge]
+    [-o overflowPopulation]
+    [-c localConfigFilePath]
+    [-d localModulesDirPath]
+    [-i initialPopulationSnapshotPath]
+    [-u updatedPopulationSnapshotPath]
+    [-t updateTimePeriodInDays]
+    [-f fixedRecordPath]
+    [-k keepMatchingPatientsPath]
+    [state [city]]:\033[0m"
 read otherSyntheaOpts 
 if [ -z "$otherSyntheaOpts" ] ; then
     otherSyntheaOpts=""
@@ -84,8 +84,8 @@ echo -e "\033[32mYou entered '$otherSyntheaOpts'.\033[0m"
 echo ""
 
 # prompt to bundle or not
-echo -e "\033[34mGenerate as bulk (ndjson) data? (Y/N) <press Enter to accept default> ["$defIsNdjsonFormat"]:\033[0m"
-read isNdjsonFormat
+read -ep $'\033[34mGenerate as bulk (ndjson) data? (Y/N) <press Enter to accept default> ['$defIsNdjsonFormat$']:\033[0m' isNdjsonFormat
+# read 
 if [ -z "$isNdjsonFormat" ] ; then
     isNdjsonFormat=$defIsNdjsonFormat
 fi
@@ -99,8 +99,8 @@ echo ""
 
 # prompt to zip the bundle if not ndjson bundles
 if [[ ${isNdjsonFormat,,} =~ ^(n)$ ]]; then
-    echo -e "\033[34mCompress [zip] the bundle? <press Enter to accept default> ["$defIsBundleCompressed"]:\033[0m"
-    read isBundleCompressed
+    read -ep $'\033[34mCompress [zip] the bundle? <press Enter to accept default> ['$defIsBundleCompressed$']:\033[0m' isBundleCompressed
+    #read isBundleCompressed
 fi
 # otherwise default to its default value
 if [ -z "$isBundleCompressed" ]; then
@@ -116,23 +116,24 @@ if [[ ! ${isBundleCompressed,,} =~ ^(y|n)$ ]] ; then
 fi
 echo ""
 
-echo "Generating synthetic patient data..."
+echo ""
+echo -e "\033[32mGenerating synthetic patient data...\033[0m"
 
-# java -jar \
-#     $SYNTHEA_JAR_PATH \
-#     -p $patientNum \
-#     -m $SYNTHEA_MODULE \
-#     --exporter.fhir.bulk_data=$( [[ ${isNdjsonFormat,,} =~ ^(y)$ ]] && echo "True" || echo "False") 
+if ! [ -f ./synthea-with-dependencies.jar ]; then
+    echo "Downloaidng the latest synthea-with-dependencies.jar..."
+    wget "https://github.com/synthetichealth/synthea/releases/download/master-branch-latest/synthea-with-dependencies.jar"
+fi
 
 java -jar \
-    $SYNTHEA_JAR_PATH \
+    ./synthea-with-dependencies.jar \
     -p $patientNum \
     $otherSyntheaOpts \
     --exporter.fhir.bulk_data=$( [[ ${isNdjsonFormat,,} =~ ^(y)$ ]] && echo "True" || echo "False") 
 
 
 echo ""
-echo "Patient data complete."
+echo -e "\033[32m...Patient data generation complete!\033[0m"
+
 echo ""
 
 # copy the generated data to the output folder in blob storage
@@ -140,7 +141,7 @@ echo ""
 if [[ ${isNdjsonFormat,,} =~ ^(n)$ && ${isBundleCompressed,,} =~ ^(y)$ ]]; then
 
     echo ""
-    echo "...Compressing (zipping) FHIR bundles..."
+    echo -e "\033[32mCompressing (zipping) FHIR bundles...\033[0m"
     mkdir -p "output/zip"
     # sudo apt-get install zip
     zip output/zip/compressedFhirBundles.zip output/fhir/*.*
@@ -162,7 +163,7 @@ fi
 
 destStorageAcct="https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$destContainer/"
 
-echo -e "\033[32m...uploading to $destStorageAcct\033[0m"
+echo -e "\033[32m...Uploading to $destStorageAcct\033[0m"
 echo ""
 
 azcopy cp "$srcUploadDir/*.*" $destStorageAcct
